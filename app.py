cda22e465a9c0f2480b9513ef76410ce5ed889d7
emmanuelmusalu <emmanuelmusalumwongela@gmail.com>
2026-03-25 20:09:30 +0300

    Fix 500 error: Dynamic course scraping and startup delay

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Add this line here
-NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
+NVIDIA_API_KEY = os.environ.get("NVIDIA_API_KEY")
 # --- FLASK-MAIL CONFIGURATION ---
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
